df53952e842db85823ba8494c412aec7646a33b7
kliang <860834240@qq.com>
2026-07-07 16:42:11 +0800

    init: C++ 项目脚手架 - CMake + vcpkg + Clang-Format + Cppcheck

---
+++ tests/main_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+TEST(testcase, test_add)
+{
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(sub(1, 2), -1);
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "run google test --> " << std::endl << std::endl;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
